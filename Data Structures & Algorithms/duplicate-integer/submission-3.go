99a9b4631b2a98c3dfb0a6998fa597c9ec23a64e
Alef Castelo <alefaraujocastelo@gmail.com>
2026-07-12 23:00:55 -0600

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.go
@@ -0,0 +1,15 @@
+func hasDuplicate(nums []int) bool {
+	frequency := make(map[int]int)
+
+	for i := 0; i <= len(nums) - 1; i++ {
+		_, exists := frequency[nums[i]]
+
+		if exists {
+			return true
+		}
+
+		frequency[nums[i]] = 1
+	}
+
+	return false
+}
