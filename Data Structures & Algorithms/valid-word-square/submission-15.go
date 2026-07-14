3b9a3c8d9ead95d5d1361ee74d720ad434435798
Alef Castelo <alefaraujocastelo@gmail.com>
2026-07-13 22:02:40 -0600

    Add: valid-word-square - submission-15

---
+++ Data Structures & Algorithms/valid-word-square/submission-15.go
@@ -0,0 +1,34 @@
+func validWordSquare(words []string) bool {
+	// get the square size
+	rows, cols := len(words), 0
+	for _, row := range words {
+		length := len(row)
+		
+		if length > cols {
+			cols = length
+		}
+	}
+
+	if cols != rows {
+		return false
+	}
+
+	matrix := make([][]rune, rows)
+	for i := 0; i < rows; i++ {
+		matrix[i] = make([]rune, cols)
+		copy(matrix[i], []rune(words[i]))
+	}
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			x := matrix[i][j]
+			y := matrix[j][i]
+
+			if x != y {
+				return false
+			}
+		}
+	}
+
+	return true
+}
