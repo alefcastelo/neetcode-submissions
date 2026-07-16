120778d11caf223274f58181dc3cb5391c3f5884
Alef Castelo <alefaraujocastelo@gmail.com>
2026-07-15 23:22:30 -0600

    Add: perform-string-shifts - submission-1

---
+++ Data Structures & Algorithms/perform-string-shifts/submission-1.go
@@ -0,0 +1,27 @@
+func stringShift(s string, shift [][]int) string {
+	r := []rune(s)
+
+	for i := 0; i < len(shift); i++ {
+		direction, amount := shift[i][0], shift[i][1]
+
+		if direction == 0 {
+
+			for j := 0; j < amount; j++ {
+				firstElement := r[0]
+				r = r[1:]
+				r = append(r, firstElement)
+			}
+
+			continue
+		}
+
+
+		for j := 0; j < amount; j++ {
+			lastElement := r[len(r)-1]
+			r = r[:len(r)-1]
+			r = append([]rune{lastElement}, r...)
+		}
+	}
+
+	return string(r)
+}
