d72ccad6dfc44834f897b056faa7dbb67abedf69
Alef Castelo <alefaraujocastelo@gmail.com>
2026-08-29 15:12:40 -0700

    Add: minimum-stack - submission-18

---
+++ Data Structures & Algorithms/minimum-stack/submission-18.go
@@ -0,0 +1,36 @@
+type MinStack struct {
+	vals []int
+	min []int
+}
+
+func Constructor() MinStack {
+	return MinStack{}
+}
+
+func (this *MinStack) Push(val int) {
+	this.vals = append(this.vals, val)
+}
+
+func (this *MinStack) Pop() {
+	this.vals = this.vals[:len(this.vals) - 1]
+}
+
+func (this *MinStack) Top() int {
+	return this.vals[len(this.vals)-1]
+}
+
+func (this *MinStack) GetMin() int {
+	if len(this.vals) == 0 {
+		return 0
+	}
+
+	min := this.vals[0]
+
+	for _, val := range this.vals {
+		if val < min {
+			min = val
+		}
+	}
+	
+	return min
+}
