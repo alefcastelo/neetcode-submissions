8296d91fb3dffb562f63b3604078f5e3cb8c8644
Alef Castelo <alefaraujocastelo@gmail.com>
2026-09-19 17:49:13 -0700

    Add: three-integer-sum - submission-19

---
+++ Data Structures & Algorithms/three-integer-sum/submission-19.go
@@ -0,0 +1,43 @@
+type IntKey struct {
+	A, B, C int
+}
+
+func threeSum(nums []int) [][]int {
+	sort.Sort(sort.IntSlice(nums))
+
+	hashset := make(map[string]int)
+
+	list := [][]int{}
+
+	for fixed := 0; fixed < len(nums) - 2; fixed++ {
+
+		left, right := fixed+1, len(nums) -1
+		
+		for left < right {
+			a := nums[fixed]
+			b := nums[left]
+			c := nums[right]
+
+			sum := a + b + c 
+
+			switch {
+			case sum == 0:
+				key := fmt.Sprint([]int{a, b, c})
+				_, exists := hashset[key]
+				if !exists {
+					list = append(list, []int{a, b, c})
+					hashset[key] = 1
+				}
+				left++
+				right--
+			case sum < 0:
+				left++
+				continue
+			default:
+				right--
+			}
+		}
+	}
+
+	return list
+}
